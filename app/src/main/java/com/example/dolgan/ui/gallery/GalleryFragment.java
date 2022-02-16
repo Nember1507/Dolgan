3d6e02331f5efaab7b8f7703cd9499020a85ab75
Kebaru <kebaru@yandex.ru>
2022-02-17 03:12:36 +0900

    0.2 - added MealsFragment

--- app/src/main/java/com/example/dolgan/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/dolgan/ui/gallery/GalleryFragment.java
@@ -14,6 +14,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dolgan.R;
 import com.example.dolgan.databinding.FragmentGalleryBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GalleryFragment extends Fragment {
 
