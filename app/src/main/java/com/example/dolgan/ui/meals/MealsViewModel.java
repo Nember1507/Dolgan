03107a35b42033bd42a3ee8d81a1917d21ba4680
Kebaru <kebaru@yandex.ru>
2022-02-17 03:12:53 +0900

    0.2 - added MealsFragment

---
+++ app/src/main/java/com/example/dolgan/ui/meals/MealsViewModel.java
@@ -0,0 +1,18 @@
+package com.example.dolgan.ui.meals;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MealsViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public MealsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is meals fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
